a6cfa333864faee3ab86f18e9d14d81614e4ee2f
KhomenkoMac <maksim_khomenko@ukr.net>
2022-01-28 22:29:34 +0200

    refactor:changing proxy to anywhere cors

--- src/API/authorsService.js
+++ src/API/authorsService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class AuthorsService{
     static async getAll(){
-        const response = await axios.get('https://thingproxy.freeboard.io/fetch/https://mylibraryapi.azurewebsites.net/Authors');
+        const response = await axios.get('https://cors-anywhere.herokuapp.com/https://mylibraryapi.azurewebsites.net/Authors');
         return response.data;
     }
 }
--- src/API/booksService.js
+++ src/API/booksService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class BooksService{
     static async getAll(){
-        const response = await axios.get('https://thingproxy.freeboard.io/fetch/https://mylibraryapi.azurewebsites.net/Books');
+        const response = await axios.get('https://cors-anywhere.herokuapp.com/https://mylibraryapi.azurewebsites.net/Books');
         return response.data;
     }
 }
