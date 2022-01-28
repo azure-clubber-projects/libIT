66aae6e2511900e7f09ddaacf8e5886e0b4f8c79
KhomenkoMac <maksim_khomenko@ukr.net>
2022-01-28 17:19:32 +0200

    add:the entities' compnts show in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
 import React from "react";
+import Authors from './components/authors/authors';
+import Books from './components/books/books';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello libIT</h1>
-      <h1>Change 2</h1>
+      <h1 className="appTitle">Lib-IT</h1>
+      <Authors />
+      <Books />
     </div>
   );
 }
